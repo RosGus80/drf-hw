f573877cb0dfd30f8b703069ab9d42c5cc9399f2
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2024-02-02 17:44:13 +0300

    hw 24.2 fix

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'phonenumber_field',
-    'django-filters',
+    'django_filters',
 
     'users',
     'courses',
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from users.views import PaymentListAPIView
 
+app_name = 'users'
+
 urlpatterns = [
     path('payments/list', PaymentListAPIView.as_view(), name='payment_list'),
 ]
