2825b8a02d45e0135166e79062d0d9733b17e805
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2024-03-01 01:48:37 +0300

    Fixed users url

--- users/urls.py
+++ users/urls.py
@@ -8,10 +8,10 @@ app_name = 'users'
 
 urlpatterns = [
     path('payments/list', PaymentListAPIView.as_view(), name='payment_list'),
-    path('users/create', UserCreateAPIView.as_view(), name='user_create'),
+    path('users/create/', UserCreateAPIView.as_view(), name='user_create'),
     path('users/<int:pk>/update', UserUpdateAPIView.as_view(), name='user_update'),
     path('users/<int:pk>/view', UserRetrieveAPIView.as_view(), name='user_view'),
     path('users/list', UserListAPIView.as_view(), name='user_list'),
-    path('users/<int:pk>/delete', UserDestroyAPIView.as_view())
+    path('users/<int:pk>/delete', UserDestroyAPIView.as_view()),
 ]
 
